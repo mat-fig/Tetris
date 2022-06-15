daec3b7d140a54422164d51881dd0939bfd8ee80
Matheus Figueroa <mfigueroa1327@gmail.com>
2022-06-14 22:24:32 -0400

    Added square and color

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import GridSquare from './components/GridSquare';
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <header className="App-header">
         <h1 className="App-title">TETRIS</h1>
       </header>
+      <GridSquare color='5'/>
     </div>
   );
 }
---
+++ src/components/GridSquare.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const GridSquare = (props) => {
+    const classes = `grid-square color-${props.color}`
+
+    return (
+        <div className={classes}/>
+    )
+}
+
+
+export default GridSquare
