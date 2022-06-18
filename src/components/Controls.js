1b444a1c40dbe42656f987d753986ef448152f6d
Matheus Figueroa <mfigueroa1327@gmail.com>
2022-06-18 12:38:31 -0400

    Added new package dependencies fix. Added store and provdiers

---
+++ src/components/Controls.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+const Controls = (props) => {
+
+    return (
+        <div className="controls">
+            <button className="control-button" onClick={(e) => {}}>Left</button>
+            <button className="control-button" onClick={(e) => {}}>Right</button>
+            <button className="control-button" onClick={(e) => {}}>Rotate</button>
+            <button className="control-button" onClick={(e) => {}}>Down</button>
+        </div>
+    )
+}
+
+export default Controls
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux'
+import gameReducer from './game-reducer'
+
+const reducers = combineReducers( {game: gameReducer} )
+
+export default reducers
